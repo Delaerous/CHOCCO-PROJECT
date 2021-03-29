45de03affb4742a076ba3aa37168ca3a01fd3fe6
Delaerous <77695875+Delaerous@users.noreply.github.com>
2021-03-29 22:50:31 +0300

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,17 +1,40 @@
-const hamburgerBtn = document.querySelector(".hamburger");
-const modalMenu = document.querySelector(".fullscreen-menu");
-const body = document.body;
-const hamburgerBtnClose = document.getElementById("close");
-const hero = document.querySelector(".hero");
 
-hamburgerBtn.addEventListener("click", function () {
-  modalMenu.style.display = "flex";
-  body.style.overflow = "hidden";
-  hero.style.display = "none";
-});
 
-hamburgerBtnClose.addEventListener("click", function () {
-  modalMenu.style.display = "none";
-  body.style.overflow = "";
-  hero.style.display = "";
-});
+const btn = document.querySelector('.hamburger');
+const menu = document.querySelector('.fullscreen-menu');
+const closeBtn = menu.querySelector('.fullscreen-menu__close');
+const body = document.querySelector('body')
+
+const togle = ()=> {
+  menu.classList.toggle('active');
+  body.classList.toggle('body__block');
+}
+
+btn.addEventListener('click', togle)
+
+
+menu.addEventListener('click', (e) => {
+  const target =e.target;
+  const DataValue = target.dataset.scrollto;
+   if (target.classList.contains('menu__link'))
+  {
+    ScrolltoSec(DataValue);
+    togle();
+}
+  
+})
+const ScrolltoSec = (attr) => {
+  const elem = document.querySelector(`[data-section-id=${attr}]`);
+  window.scroll({
+    top: elem.getBoundingClientRect,
+    left: 0,
+    behavior: 'smooth'}
+  )
+}
+
+closeBtn.addEventListener('click', (e) => {
+  if (closeBtn.classList.contains('fullscreen-menu__close')) {
+    togle()
+  }
+})
+
